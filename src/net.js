8e50a60446be4b35ce5daec40e64e036c234ad75
洛也 <luoyefe@gmail.com>
2020-10-15 11:17:02 +0800

    feat: 优化 ping 断网检测逻辑

--- src/net.js
+++ src/net.js
@@ -1,36 +1,40 @@
 const EventEmitter = require('events').EventEmitter;
-const spawn = require('child_process').spawn;
-const rl = require('readline');
+const ping = require('ping')
 
-const RE_SUCCESS = /bytes from/i;
-const IP = '223.5.5.5';
+const HOST = '223.5.5.5';
 
-const proc = spawn('ping', [IP]);
-
-const rli = rl.createInterface(proc.stdout, proc.stdin);
 const network = new EventEmitter();
 
 network.online = false;
 
-let offlineCount = 0
+async function main() {
+  try {
+    let { alive } = await ping.promise.probe(HOST, {
+      timeout: 2,
+    });
 
-rli.on('line', function (str) {
-  if (RE_SUCCESS.test(str)) {
-    if (!network.online) {
+    if (alive && !network.online) {
       network.online = true;
       network.emit('online');
     }
-  } else if (network.online) {
-    offlineCount += 1
 
-    // ping 五次错误判断为断腕
-    if (offlineCount === 5) {
+    if (!alive && network.online) {
       network.online = false;
       network.emit('offline');
-      offlineCount = 0
     }
+
+    setTimeout(main, 1000)
+  } catch (e) {
+    if (network.online) {
+      network.online = false;
+      network.emit('offline');
+    }
+
+    setTimeout(main, 1000)
   }
-});
+}
+
+main()
 
 module.exports = network
 
